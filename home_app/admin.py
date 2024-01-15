182e46cf917691bc28ba468d8e31018c39bdd1bb
Wanwisa Waddington <kaywanwisa4@googlemail.com>
2024-01-15 08:31:03 +0000

    Create class GalleryAdmin in admin.py file to add it to admin panel for the admin to add multiple image

--- home_app/admin.py
+++ home_app/admin.py
@@ -1,13 +1,23 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Gallery
+
+
+
+class GalleryAdminTab(admin.TabularInline):
+    model = Gallery
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     
     inlines = [GalleryAdminTab]
-    list_display = ('title', 'slug', 'status', 'created_on', 'views', 'id_post')
+    list_display = ('user', 'title', 'slug', 'status', 'created_on', 'views', 'visibility', 'id_post')
     search_fields = ['title', 'content', 'id_post']
     prepopulated_fields = {'slug': ('title',)}
     list_filter = ('status', 'created_on')
-    
+
+
+@admin.register(Gallery)
+class GalleryAdmin(admin.ModelAdmin):
+    list_editable = ['active']
+    list_display = ['post', 'active']
