5ffc271e0ef56ee51ab5257348b88677ea5eeace
Wanwisa Waddington <kaywanwisa4@googlemail.com>
2024-01-15 08:26:42 +0000

    Create class PostAdmin in admin.py

--- home_app/admin.py
+++ home_app/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from .models import Post
 
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    
+    inlines = [GalleryAdminTab]
+    list_display = ('title', 'slug', 'status', 'created_on', 'views', 'id_post')
+    search_fields = ['title', 'content', 'id_post']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
+    
