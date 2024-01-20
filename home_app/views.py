d6ae052f556355d984dd1937d192941b848e6fe1
Wanwisa Waddington <kaywanwisa4@googlemail.com>
2024-01-20 13:08:39 +0000

    Add method  popular to class PostList in views.py file

--- home_app/views.py
+++ home_app/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Post
-
+from .models import Popular
 
 class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by('created_on')
     template_name = 'index.html'
+
+    def popular(request):
+        places = Popular.objects.all()
+        return render(request, 'home_app/popular.html', {'places': places})
