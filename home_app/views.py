5df95300dd6f2ee87c33eea9a3c490f04036f5a9
Wanwisa Waddington <kaywanwisa4@googlemail.com>
2024-01-21 15:57:23 +0000

    Apply if statement ti method index and method accont

--- home_app/views.py
+++ home_app/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render,redirect
 from django.views import generic
 from .models import Post
-from .models import Popular
-from .forms import AccountForm
+from .models import Popular,Profile,Gallery
+from .forms import AccountForm,UserPostForm
 
 
 
@@ -12,8 +12,27 @@ def login(request):
 
 
 def index(request):
-    places = Popular.objects.all()
-    return render(request, 'home_app/index.html', {})
+    if request.method == 'POST':
+        form = UserPostForm(request.POST)
+        if form.is_valid():
+            post_instance = form.save(commit=False)
+            post_instance.user = request.user
+            post_instance.save()
+
+    
+            images = request.FILES.getlist('images')
+            for image in images:
+                Gallery.objects.create(post=post_instance, image=image)
+
+            
+    else:
+        form = UserPostForm()
+
+
+    posts = Post.objects.all() #filter(user=request.user)
+    return render(request, 'home_app/index.html', {
+        'posts' : posts
+    })
 
 
 
@@ -21,14 +40,35 @@ def index(request):
 
 def account(request):
     if request.method == 'POST':
+        print(request.POST)
         form = AccountForm(request.POST, request.FILES)
         if form.is_valid():
-            return redirect('success_page') 
+            profile = Profile.objects.get(user=request.user)
+            if form.cleaned_data['profileimage'] != None:
+                profile.profile_img = form.cleaned_data['profileimage']
+            profile.phone = form.cleaned_data['phone']
+            profile.save()
+            request.user.first_name = form.cleaned_data['first_name']
+            request.user.last_name = form.cleaned_data['last_name']
+            request.user.save()
+            return render(request, 'home_app/account.html', {'form': form})
     else:
         form = AccountForm()
+        user = request.user
+        try:
+            profile = Profile.objects.get(user=request.user)
+        except Profile.DoesNotExist:
+            profile = Profile.objects.create(user=request.user,id_user=request.user.id)
+        form.fields['profileimage'].initial = profile.profile_img
+        form.fields['phone'].initial = profile.phone
+        form.fields['accountname'].initial = user.username
+        form.fields['first_name'].initial = user.first_name
+        form.fields['last_name'].initial = user.last_name
+        form.fields['email'].initial = user.email
+        
 
     return render(request, 'home_app/account.html', {'form': form})
 
 def popular(request):
     places = Popular.objects.all()
-    return render(request, 'home_app/popular.html', {'places': places})
+    return render(request, 'home_app/most_popular_place.html', {'places': places})
