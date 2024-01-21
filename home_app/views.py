83e20ea3370c5baefcfcff5f697d9864981f0eae
Wanwisa Waddington <kaywanwisa4@googlemail.com>
2024-01-21 06:00:46 +0000

    Fix syntex error in views.py file in home_app directory

--- home_app/views.py
+++ home_app/views.py
@@ -1,12 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views import generic
 from .models import Post
 from .models import Popular
+from .forms import AccountForm
 
-class PostList(generic.ListView):
-    model = Post
-    queryset = Post.objects.filter(status=1).order_by('created_on')
-    template_name = 'index.html'
+
+
+def login(request):
+    return render(request, 'home_app/login.html', {})
+
+
+
+def index(request):
+    places = Popular.objects.all()
+    return render(request, 'home_app/index.html', {})
+
+
+
+
+
+def account(request):
+    if request.method == 'POST':
+        form = AccountForm(request.POST, request.FILES)
+        if form.is_valid():
+            return redirect('success_page') 
+    else:
+        form = AccountForm()
+
+    return render(request, 'home_app/account.html', {'form': form})
 
 def popular(request):
     places = Popular.objects.all()
