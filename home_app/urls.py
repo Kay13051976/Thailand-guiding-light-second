57c5fc0cd9e8e0016fd1f5c43b159458f8d73c7e
Wanwisa Waddington <kaywanwisa4@googlemail.com>
2024-01-21 15:54:50 +0000

    Remove path from urlpatterns in urls.py file

--- home_app/urls.py
+++ home_app/urls.py
@@ -1,12 +1,10 @@
 from . import views
 from django.urls import path
-from .views import popular,index,account,login
+from .views import popular,index,account
 
 
 urlpatterns = [
     path('account/', account, name='account'),
     path('index/', index, name='index'),
-    path('account/', index, name='account'),
-    path('login/', login, name='login'),
     path('popular/', popular, name='popular'),
 ]
