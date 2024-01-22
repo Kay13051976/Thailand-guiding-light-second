8a35067970469315b307780a60987830c8c293ff
Wanwisa Waddington <kaywanwisa4@googlemail.com>
2024-01-22 14:55:58 +0000

    Create class CustomSignupForm to custom Signup form

--- home_app/forms.py
+++ home_app/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Post,VISIBILITY
 
+from allauth.account.forms import SignupForm
 
 class UserPostForm(forms.ModelForm):
 
@@ -18,3 +19,15 @@ class AccountForm(forms.Form):
     last_name = forms.CharField(label="Lastname",max_length=30)
     email = forms.EmailField(label="Email")
     phone = forms.CharField(max_length=15,label="Phone Number",)
+
+
+
+
+class CustomSignupForm(SignupForm):
+    first_name = forms.CharField(max_length=30, label='First Name')
+    last_name = forms.CharField(max_length=30, label='Last Name')
+
+    # password1 = forms.CharField(widget=forms.PasswordInput, label='Password',min_length=8)
+    # password2 = forms.CharField(widget=forms.PasswordInput, label='Confirm Password',min_length=8)
+
+   
