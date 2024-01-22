9b6fb796df6e6d261bec6b0584813b931a6d24f3
Wanwisa Waddington <kaywanwisa4@googlemail.com>
2024-01-22 14:38:00 +0000

    Add admin_approved to lis_display for class PostAdmin in admin.py in home_app

--- home_app/admin.py
+++ home_app/admin.py
@@ -22,7 +22,7 @@ class GalleryAdminTab(admin.TabularInline):
 class PostAdmin(admin.ModelAdmin):
     
     inlines = [GalleryAdminTab]
-    list_display = ('user', 'title', 'slug', 'status', 'created_on', 'views', 'visibility', 'id_post')
+    list_display = ('user', 'title', 'slug', 'status', 'created_on', 'views', 'visibility', 'id_post','admin_approved')
     search_fields = ['title', 'content', 'id_post']
     prepopulated_fields = {'slug': ('title',)}
     list_filter = ('status', 'created_on')
