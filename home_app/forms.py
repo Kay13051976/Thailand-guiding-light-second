8f99dbe5c58f28a7ad951f829fd8fd22521e3499
Wanwisa Waddington <kaywanwisa4@googlemail.com>
2024-01-24 06:35:47 +0000

    Fix label name in class AccountForm in form.py

--- home_app/forms.py
+++ home_app/forms.py
@@ -15,8 +15,8 @@ class UserPostForm(forms.ModelForm):
 class AccountForm(forms.Form):
     profileimage = forms.ImageField(label="Profile Image",required=False)
     accountname = forms.CharField(label="Account Name",max_length=100)
-    first_name = forms.CharField(label="Firstame",max_length=30)
-    last_name = forms.CharField(label="Lastname",max_length=30)
+    first_name = forms.CharField(label="First Name",max_length=30)
+    last_name = forms.CharField(label="Last Name",max_length=30)
     email = forms.EmailField(label="Email")
     phone = forms.CharField(max_length=15,label="Phone Number",)
 
