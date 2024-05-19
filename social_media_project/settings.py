d871896f615b3bd786fe384cbd5e2245b93cd89f
Wanwisa Waddington <kaywanwisa4@googlemail.com>
2024-05-19 21:15:16 +0000

    Fixed typo in settings.py

--- social_media_project/settings.py
+++ social_media_project/settings.py
@@ -26,11 +26,11 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = False
-# os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+DEBUG = False
+#os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ['8000-kay13051976-thailandgui-jfkilhjpyib.ws-eu107.gitpod.io',
                  'thailand-guiding-light-2fb0b0e33db8.herokuapp.com',
