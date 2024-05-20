885f60ea2f4faf95e4bfff7c4adb2e67279edf49
Wanwisa Waddington <kaywanwisa4@googlemail.com>
2024-05-20 04:09:38 +0000

    Added STATICFILES_STORAGE to cloudinary_storage in settings.py

--- social_media_project/settings.py
+++ social_media_project/settings.py
@@ -67,11 +67,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'crispy_forms',
     'crispy_bootstrap5',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary',
     'allauth',
     'allauth.account',
-    'cloudinary_storage',
-    'cloudinary',
     'home_app',
 
 ]
@@ -174,13 +174,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR
-# DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
